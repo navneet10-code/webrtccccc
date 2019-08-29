9a4e1043af0728f83e13cccbb3ef1ff1ee14e248
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-08-29 17:11:23 +0530

    Update CallModal.js

--- client/src/js/CallModal.js
+++ client/src/js/CallModal.js
@@ -5,12 +5,12 @@ import classnames from 'classnames';
 
 
 function CallModal({ status, callFrom, startCall, rejectCall}) {
-  
+  function abc{
   if(status == 'active') {
   var audio = new Audio('http://hipehome.com/nokia-ringtone-2019-256k-46684.mp3');
     audio.play();
   }
-                 
+    }  
   
    const acceptWithVideo = (video) => {
     const config = { audio: true, video , audio};
