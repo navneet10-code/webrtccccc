62e635975381c8949e344722d3cfbfc0fa4e0cce
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-02 17:55:49 +0530

    Update CallWindow.js

--- client/src/js/CallWindow.js
+++ client/src/js/CallWindow.js
@@ -75,7 +75,7 @@ display.textContent = minutes + ":" + seconds;
 
 if (--timer < 0) {
 timer = duration;console.log('ooooooooooooooooooooooooooooo');
-setTimeout('endCall()', 5000 )
+setTimeout(endCall(), 5000 )
 
 }
   else{console.log('Tgggggggggggggggggggggggggggggggggggg');}
