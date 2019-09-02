2cce6a787ad82fdcf72f9799c3f5ad78305c65cb
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-02 12:58:04 +0530

    Update CallWindow.js

--- client/src/js/CallWindow.js
+++ client/src/js/CallWindow.js
@@ -49,43 +49,6 @@ class CallWindow extends Component {
     if (this.peerVideo && peerSrc) this.peerVideo.srcObject = peerSrc;
     if (this.localVideo && localSrc) this.localVideo.srcObject = localSrc;
   }
-   
-  
-  
-  
-  countdownTimeStart(){
-
-var countDownDate = new Date("Sep 25, 2025 15:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = hours + "h "
-    + minutes + "m " + seconds + "s ";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
-}
-  
-  
-  
-  
   
   
   
@@ -116,7 +79,7 @@ var x = setInterval(function() {
   }
 
   render() {
-    const { status, endCall, countdownTimeStart} = this.props;
+    const { status, endCall} = this.props;
     return (
       <div className={classnames('call-window', status)}>
         <video id="peerVideo" ref={el => this.peerVideo = el} autoPlay />
