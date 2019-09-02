d1e247ad921e14f2b3a530a58c96edf08d430ab9
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-02 12:32:12 +0530

    Update CallWindow.js

--- client/src/js/CallWindow.js
+++ client/src/js/CallWindow.js
@@ -53,43 +53,35 @@ class CallWindow extends Component {
   
   
   
-  
-  startTimer(duration, display) {
-    var start = Date.now(),
-        diff,
-        minutes,
-        seconds;
-    function timer() {
-        // get the number of seconds that have elapsed since 
-        // startTimer() was called
-        diff = duration - (((Date.now() - start) / 1000) | 0);
+  countdownTimeStart(){
 
-        // does the same job as parseInt truncates the float
-        minutes = (diff / 60) | 0;
-        seconds = (diff % 60) | 0;
+var countDownDate = new Date("Sep 25, 2025 15:00:00").getTime();
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-        display.textContent = minutes + ":" + seconds; 
-
-        if (diff <= 0) {
-            // add one second so that the count down starts at the full duration
-            // example 05:00 not 04:59
-            start = Date.now() + 1000;
-        }
-    };
-    // we don't want to wait a full second before the timer starts
-    timer();
-    setInterval(timer, 1000);
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
 }
-
-window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
-  
   
   
   
@@ -131,27 +123,12 @@ window.onload = function () {
         <video id="localVideo" ref={el => this.localVideo = el} autoPlay muted />
         <div className="video-control">
            
-       <div><span id="time"></span> minutes!</div>
-
-       <div class="clock-builder-output"></div>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script type="text/javascript" src="/assets/clock_assets/jquery.countdown.min.js"></script>
-<script type="text/javascript">
-$(function(){
-  d = new Date();
-	d.setTime(1567491540*1000); // from: 09/03/2019 11:49 am +0530
-	$('.clock-builder-output').countdown(d, function(event) {
-		$(this).html(event.strftime('%H:%M:%S'));
-	});
-});
-</script>
-
-
-
-
-
-
-
+        <button id="demo"
+            type="button"
+            className="btn-action hangup fa fa-phone"
+            onClick={() => countdownTimeStart(true)}
+          />
+        
 
     
           {this.renderControlButtons()}
