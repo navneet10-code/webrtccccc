2237c3022550d5a0b4ffd4047a252d3d83dc4d11
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-04 12:38:02 +0530

    Update app.js

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -69,6 +69,51 @@ class App extends Component {
       peerSrc: null
     });
   }
+  
+  
+
+  
+  record() {
+if(typeof RecordRTC_Extension === 'undefined') {
+    alert('RecordRTC chrome extension is either disabled or not installed.');
+}
+
+// first step
+var recorder = new RecordRTC_Extension();
+
+var video = document.querySelector('video');
+
+function stopRecordingCallback(blob) {
+    video.src = video.srcObject = null;
+    video.src = URL.createObjectURL(blob);
+    console.log('value',video.src);
+    recorder = null;
+}
+
+document.getElementById('btn-start-recording').onclick = function() {
+    this.disabled = true;
+    // you can find list-of-options here:
+    // https://github.com/muaz-khan/Chrome-Extensions/tree/master/screen-recording#getsupoortedformats
+    var options = recorder.getSupoortedFormats()[1];
+
+    // second step
+    recorder.startRecording(options, function() {
+        document.getElementById('btn-stop-recording').disabled = false;
+    });
+};
+
+document.getElementById('btn-stop-recording').onclick = function() {
+    this.disabled = true;
+
+    // third and last step
+    recorder.stopRecording(stopRecordingCallback);
+};
+}
+  
+  
+  
+  
+  
 
   render() {
     const { clientId, callFrom, callModal, callWindow, localSrc, peerSrc } = this.state;
