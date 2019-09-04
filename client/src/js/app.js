19f6010dd570e08ab3654f64b49a730fab5cb8a3
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-04 12:45:48 +0530

    Update app.js

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -74,39 +74,9 @@ class App extends Component {
 
   
   record() {
-if(typeof RecordRTC_Extension === 'undefined') {
+
     alert('RecordRTC chrome extension is either disabled or not installed.');
-}
-
-// first step
-var recorder = new RecordRTC_Extension();
-
-var video = document.querySelector('video');
-
-function stopRecordingCallback(blob) {
-    video.src = video.srcObject = null;
-    video.src = URL.createObjectURL(blob);
-    console.log('value',video.src);
-    recorder = null;
-}
-
-
-    this.disabled = true;
-    // you can find list-of-options here:
-    // https://github.com/muaz-khan/Chrome-Extensions/tree/master/screen-recording#getsupoortedformats
-    var options = recorder.getSupoortedFormats()[1];
-
-    // second step
-    recorder.startRecording(options, function() {
-        document.getElementById('btn-stop-recording').disabled = false;
-    });
-
-
-
-    this.disabled = true;
-
-    // third and last step
-    recorder.stopRecording(stopRecordingCallback);
+    console.log('inside');
 
 }
   
