ead490346927850d4a9c74d845d8fc4f8a1961a8
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-10 18:24:11 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -5,20 +5,14 @@ let friendID;
 
 
  
-var pg = require('pg');
+var connectionString = "postgres://fyngdzukkahhbq:1961b06d70494c1ca70d54fbc09eac2edeaa687f0d4f828cca4ec94052d22eea@ec2-54-221-212-126.compute-1.amazonaws.com:5432/dd1i094ii9uj7m"
 
-app.get('/db', function (request, response) {
-    pg.defaults.ssl = true;
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
-        if (err) throw err;
-        console.log('Connected to postgres! Getting schemas...');
-
-        client
-            .query('SELECT table_schema,table_name FROM information_schema.tables;')
-            .on('row', function(row) {
-                console.log(JSON.stringify(row));
-            });
-    });
+pg.connect(connectionString, function(err, client, done) {
+   client.query('SELECT * FROM your_table', function(err, result) {
+      done();
+      if(err) return console.error(err);
+      console.log(result.rows);
+   });
 });
 
 
