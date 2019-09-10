f93715cba38f5a03904fb4661c717e493d608f73
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-10 13:19:25 +0530

    Update socket.js

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -9,13 +9,13 @@ function initSocket(socket) {
   let id;
   socket
     .on('init', async () => {
-      //id = await users.create(socket);
+      id = await users.create(socket);
       socket.emit('init', { id });
     })
     .on('request', (data) => {
       const receiver = users.get(data.to);
       if (receiver) {
-        receiver.emit('request', { from: saasnic });
+        receiver.emit('request', { from: id });
       }
     })
     .on('call', (data) => {
