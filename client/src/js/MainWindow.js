fee466c86f0ccd7b4a4c17832f429b6685e06c70
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-11 10:54:20 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -4,20 +4,6 @@ import PropTypes from 'proptypes';
 let friendID;
 
 
- 
-var connectionString = "postgres://fyngdzukkahhbq:1961b06d70494c1ca70d54fbc09eac2edeaa687f0d4f828cca4ec94052d22eea@ec2-54-221-212-126.compute-1.amazonaws.com:5432/dd1i094ii9uj7m"
-
-pg.connect(connectionString, function(err, client, done) {
-   client.query('SELECT * FROM your_table', function(err, result) {
-      done();
-      if(err) return console.error(err);
-      console.log(result.rows);
-   });
-});
-
-
-
-
 
 class MainWindow extends Component {
   /**
