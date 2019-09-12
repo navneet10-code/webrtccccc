bc214525f17ad0759e6e9bcb9f2c0006b690d1d6
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-12 11:53:10 +0530

    Update CallWindow.js

--- client/src/js/CallWindow.js
+++ client/src/js/CallWindow.js
@@ -17,6 +17,32 @@ var recorder ;
 var blobs = [];
 
 
+
+
+let pg = require('pg');
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = true;
+}
+
+// include an OR statement if you switch between a local dev db and 
+// a remote heroku environment
+
+let connString = process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:localpostgresport/yourlocaldbname';
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString : connString
+});
+
+
+
+
+
+
+
+
+
+
 class CallWindow extends Component {
 constructor(props) {
 super(props);
