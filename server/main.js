3468f0c2f64b4294f84eeb4484d8a6ef7d208878
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-12 12:07:36 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,6 +1,60 @@
 const config = require('../config.json');
 const server = require('./lib/server');
 
+
+var express = require("express");
+var mysql = require('mysql');
+var app = express();
+app.use(express.logger());
+
+var db_config = {
+    host : 'sql12.freemysqlhosting.net',
+database : 'sql12304794',
+user : 'sql12304794',
+password : 'PLSEEGHnWv'
+};
+
+var connection;
+
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+            handleDisconnect();                      	// lost due to either server restart, or a
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
+app.get('/', function(request, response) {
+    connection.query('SELECT * from user', function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+});
+
+
+
+
+
+
 config.PORT = process.env.PORT || config.PORT;
 
 server.run(config);
